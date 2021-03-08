f9564111a19ccbb4a668fb963218dc7e2fd24348
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-08 13:45:03 +0100

    Task 5) Refactor test code

--- test/lab/OptionalsTest.scala
+++ test/lab/OptionalsTest.scala
@@ -2,10 +2,18 @@ package lab
 
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
+import lab.Optionals.Option
 import lab.Optionals.Option._
 
 class OptionalsTest {
+  val emptyOptional: Option[Int] = None()
+  val optionalValue = 11
+  val optional: Option[Int] = Some(optionalValue)
+
   @Test def filterOnEmptyOptionalTest(): Unit = {
-    assertEquals(None(), filter(None[Int](), (_: Int) > 3))
+    val predicate: Int => Boolean = _ > 3
+    assertEmpty(filter(emptyOptional, predicate))
   }
+
+  def assertEmpty[A](optional: Option[A]): Unit = assertTrue(isEmpty(optional))
 }

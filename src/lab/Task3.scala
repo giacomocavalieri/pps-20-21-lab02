eacb16d98ae654023f1894a6c16467b85b750d9b
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-08 12:10:14 +0100

    Task 3) tail recursive fibonacci tests and implementation

--- src/lab/Task3.scala
+++ src/lab/Task3.scala
@@ -1,9 +1,21 @@
 package lab
 
+import scala.annotation.tailrec
+
 object Task3 {
   def fib(n: Int): Int = n match {
     case 0 => 0
-    case 1 | 2 => 1
+    case 1 => 1
     case n if n > 0 => fib(n-1) + fib(n-2)
+  }
+
+  def fibTail(n: Int): Int = {
+    @tailrec
+    def _fib(n: Int, first: Int, second: Int): Int = n match {
+      case 0 => first
+      case 1 => second
+      case n if n > 0 => _fib(n-1, second, first + second)
+    }
+    _fib(n, 0, 1)
   }
 }
--- test/lab/Task3Test.scala
+++ test/lab/Task3Test.scala
@@ -1,17 +1,27 @@
 package lab
 
-import lab.Task3.fib
+import lab.Task3._
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
 class Task3Test {
-  @Test def testFirstFibonacciNumbers() {
+  @Test def testFibonacci() {
+    testFirstFibonacciNumbers(fib)
+    testFibonacciNumbers(fib)
+  }
+
+  @Test def testTailRecursiveFibonacci(): Unit = {
+    testFirstFibonacciNumbers(fibTail)
+    testFibonacciNumbers(fibTail)
+  }
+
+  def testFirstFibonacciNumbers(fib: Int => Int) {
     assertEquals(0, fib(0))
     assertEquals(1, fib(1))
     assertEquals(1, fib(2))
   }
 
-  @Test def testFibonacciNumbers(): Unit = {
+  def testFibonacciNumbers(fib: Int => Int): Unit = {
     assertEquals(3, fib(4))
     assertEquals(13, fib(7))
   }

a206b9906c5d206fecb9107bce805750b8686b93
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-08 13:31:46 +0100

    Task 5) Added default code provided by exercise

---
+++ src/lab/Optionals.scala
@@ -0,0 +1,24 @@
+package lab
+
+object Optionals {
+  sealed trait Option[A]
+  object Option {
+    case class None[A]() extends Option[A]
+    case class Some[A](a: A) extends Option[A]
+
+    def isEmpty[A](opt: Option[A]): Boolean = opt match {
+      case None() => true
+      case _ => false
+    }
+
+    def getOrElse[A, B >: A](opt: Option[A], orElse: B): B = opt match {
+      case Some(a) => a
+      case _ => orElse
+    }
+
+    def flatMap[A, B](opt: Option[A])(f: A => Option[B]): Option[B] = opt match {
+      case Some(a) => f(a)
+      case _ => None()
+    }
+  }
+}

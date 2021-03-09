dd0ad84462103d35ad2aaed38714126f5ef2ed55
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-09 22:10:29 +0100

    Task 4) Simplify tests

--- test/lab/ShapesTest.scala
+++ test/lab/ShapesTest.scala
@@ -1,6 +1,5 @@
 package lab
 
-import Shapes.Shape
 import Shapes.Shape._
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
@@ -11,34 +10,22 @@ class ShapesTest {
   @Test def testCircle() {
     val circleRadius = 11.1
     val circle = Circle(circleRadius)
-    testShapePerimeter(math.Pi * 2 * circleRadius, circle)
-    testShapeArea(math.Pi * circleRadius * circleRadius, circle)
+    assertEquals(math.Pi * 2 * circleRadius, perimeter(circle), doubleDelta)
+    assertEquals(math.Pi * circleRadius * circleRadius, area(circle), doubleDelta)
   }
 
-  @Test def testRectanglePerimeter() {
+  @Test def testRectangle() {
     val edge1 = 11.1
     val edge2 = 1.11
     val rectangle = Rectangle(edge1, edge2)
-    testShapePerimeter((edge1 + edge2) * 2, rectangle)
-    testShapeArea(edge1 * edge2, rectangle)
+    assertEquals((edge1 + edge2) * 2, perimeter(rectangle), doubleDelta)
+    assertEquals(edge1 * edge2, area(rectangle), doubleDelta)
   }
 
-  @Test def testSquarePerimeter(): Unit = {
+  @Test def testSquare(): Unit = {
     val edge = 11.1
     val square = Square(edge)
-    testShapePerimeter(edge * 4, square)
-    testShapeArea(edge * edge, square)
-  }
-
-  def testShapePerimeter(expected: Double, shape: Shape) {
-    testShapeMap(expected, shape, perimeter)
-  }
-
-  def testShapeArea(expected: Double, shape: Shape) {
-    testShapeMap(expected, shape, area)
-  }
-
-  def testShapeMap(expected: Double, shape: Shape, map: Shape => Double) {
-    assertEquals(expected, map(shape), doubleDelta)
+    assertEquals(edge * 4, perimeter(square), doubleDelta)
+    assertEquals(edge * edge, area(square), doubleDelta)
   }
 }

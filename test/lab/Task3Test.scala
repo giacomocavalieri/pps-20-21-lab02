ec7d92c1733d890e6686445f025a8be91938cd35
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-09 18:30:05 +0100

    Task 3) Refactor test code: merge two test methods in one

--- test/lab/Task3Test.scala
+++ test/lab/Task3Test.scala
@@ -6,22 +6,17 @@ import org.junit.jupiter.api.Test
 
 class Task3Test {
   @Test def testFibonacci() {
-    testFirstFibonacciNumbers(fib)
     testFibonacciNumbers(fib)
   }
 
   @Test def testTailRecursiveFibonacci(): Unit = {
-    testFirstFibonacciNumbers(fibTail)
     testFibonacciNumbers(fibTail)
   }
 
-  def testFirstFibonacciNumbers(fib: Int => Int) {
+  def testFibonacciNumbers(fib: Int => Int) {
     assertEquals(0, fib(0))
     assertEquals(1, fib(1))
     assertEquals(1, fib(2))
-  }
-
-  def testFibonacciNumbers(fib: Int => Int): Unit = {
     assertEquals(3, fib(4))
     assertEquals(13, fib(7))
   }
